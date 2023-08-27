99a5161c777df63596ba4f73f7f115f4f3b992c7
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-08-27 17:31:11 +0200

    feat: Nav component

---
+++ components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const Nav = () => {
+  return (
+    <nav>
+        <Link
+            href=""
+        >
+            Tableau de bord
+        </Link>
+        <Link
+            href=""
+        >
+            Nouvelle candidature
+        </Link>
+        <Link
+            href=""
+        >
+            Offres
+        </Link>
+    </nav>
+  )
+}
+
+export default Nav
