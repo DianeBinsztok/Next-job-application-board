8538afb310888fdaaa0c4c6bef4499929c1cc3e3
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-09-19 15:47:17 +0200

    feat: link to /application-detail

--- components/ApplicationListItem.jsx
+++ components/ApplicationListItem.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const ApplicationListItem = () => {
   return (
-    <li>
-        <h3>Titre de l'offre</h3>
-        <div>Entreprise</div>
-        <div>Ville</div>
-        <div>Type de contrat</div>
-        <div>Statut de la candidature</div>
-    </li>
+    
+        <Link
+            href="/application-detail"
+        >
+            <li>
+            <h3>Titre de l'offre</h3>
+            <div>Entreprise</div>
+            <div>Ville</div>
+            <div>Type de contrat</div>
+            <div>Statut de la candidature</div>
+            </li>
+        </Link>
+    
   )
 }
 
