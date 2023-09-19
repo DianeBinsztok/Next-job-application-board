d48813fcd933ab3c1553fc3907b7423801f5f5d0
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2023-09-19 15:24:35 +0200

    pull main routes

--- app/new-application/page.jsx
+++ app/new-application/page.jsx
@@ -1,7 +1,7 @@
 export default function page() {
     return (
       <main>
-        <h1>Page de détail: candidatures</h1>
+        <h1>Page de détail: nouvelle candidature</h1>
       </main>
     )
   }
